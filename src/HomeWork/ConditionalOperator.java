169732d1520a55ee4dd669ca3f28a400d2179ea3
Ivan-Nikolaenko <inikolaenko@leantech.ai>
2023-02-07 14:44:35 +0600

    +

---
+++ src/HomeWork/ConditionalOperator.java
@@ -0,0 +1,17 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int temp = in.nextInt();
+        if (temp > 500) {
+            System.out.println("Pizzzza");
+        } else if (temp < 100) {
+        } else {
+            System.out.println("5ka");
+        }
+
+    }
+}
