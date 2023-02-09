759f70df41a25095dea5239c1b6be0d616981e83
Ivan-Nikolaenko <inikolaenko@leantech.ai>
2023-02-09 19:37:54 +0600

    +

---
+++ src/JavaSyntax/AverageDcim.java
@@ -0,0 +1,16 @@
+package JavaSyntax;
+
+public class AverageDcim {
+    public static void main(String[] args) {
+        int count=0;
+        int summa=0;
+        for (int i = 1; i <=100 ; i++) {
+            System.out.println(i);
+            summa+=i;
+            count++;
+        }
+        System.out.println(summa);
+        System.out.println(count);
+        System.out.println(summa/count);
+    }
+}
